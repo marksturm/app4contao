2e5f3d07daa43ba2bbbb7adf97da259b547f7ca0
Mark Sturm <privat@mark-sturm.de>
2016-07-14 11:07:34 +0200

    ContaoaNewsApi

---
+++ contao/system/modules/newsapp4contao/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Front end modules
+ */
+
+$GLOBALS['FE_MOD']['newsapp4contao']['json_newslist'] = 'ModuleNewsListJson';
+$GLOBALS['FE_MOD']['newsapp4contao']['json_newsreader'] = 'ModuleNewsReaderJson';
---
+++ contao/system/modules/newsapp4contao/languages/de/modules.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['FMD']['newsapp4contao'] = array('NewsApp4Contao'); 
+$GLOBALS['TL_LANG']['FMD']['json_newsreader'] = array('Nachrichtenleser', 'Dieses Modul gibt einen Nachrichtenbeitrag im json-Format aus.'); 
+$GLOBALS['TL_LANG']['FMD']['json_newslist'] = array('Nachrichtenliste', 'Dieses Modul gibt eine Nachrichtenliste aus im json-Format aus.');
